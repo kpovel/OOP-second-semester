e360be6dec4d7b5b14f239bedb09eba4b2ccaee1
Pavlo Karas <kar.pavel123@gmail.com>
2023-02-15 13:16:44 +0200

    Did the first task

---
+++ 10th-laboratory-work/tenth-LW-first-task/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace tenth_LW_first_task;
+
+internal abstract class FirstTask
+{
+    private static void Main()
+    {
+        GenerateNewArray(out var array);
+        SwapArrayElements(array);
+    }
+
+    private static void SwapArrayElements(IList<int> array)
+    {
+        var indexOfSmallestElement = 0;
+        var indexOfBiggestElement = 0;
+
+        for (var i = 1; i < array.Count; i++)
+        {
+            if (array[i] < array[indexOfSmallestElement]) indexOfSmallestElement = i;
+            if (array[i] > array[indexOfBiggestElement]) indexOfBiggestElement = i;
+        }
+        
+        (array[indexOfSmallestElement], array[indexOfBiggestElement]) = 
+            (array[indexOfBiggestElement], array[indexOfSmallestElement]);
+        
+        Console.WriteLine("\nSwapped array:");
+        foreach (var i in array)
+        {
+            Console.Write($"{i} ");
+        }
+    }
+
+    private static void GenerateNewArray(out int[] array)
+    {
+        array = new int[10];
+        var randomNumber = new Random();
+        for (var i = 0; i < array.Length; i++)
+        {
+            array[i] = randomNumber.Next(-10, 10);
+        }
+
+        Console.WriteLine("Generated array:");
+        foreach (var i in array)
+        {
+            Console.Write($"{i} ");
+        }
+    }
+}
